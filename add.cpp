311fcd695418855a73ffe66a40ee7d285615f0f2
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2022-10-16 14:56:32 +0530

    Addition of number

---
+++ add.cpp
@@ -0,0 +1,12 @@
+//to add 2 numbers given by users
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers="<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition of given 2 numbers is "<<c;
+    return 0;
+}
