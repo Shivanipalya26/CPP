8c5d7e1d9fb6edb2cc41b0e009ef3827c9165d76
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2022-10-16 15:07:08 +0530

    Area of circle

---
+++ area_circle.cpp
@@ -0,0 +1,14 @@
+//to find area of circle
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float r,area;
+    cout<<"Enter radius of circle"<<endl;
+    cin>>r;
+    area = 3.14*r*r;
+    cout<<"Area of triangle is "<<area;
+    return 0;
+}
