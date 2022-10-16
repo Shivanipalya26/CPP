9353d3f45a673b65c10f5c6eaa25d33655fba48c
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2022-10-16 15:00:22 +0530

    Area of triangle

---
+++ area_triangle.cpp
@@ -0,0 +1,14 @@
+//to find area of triangle
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float b,h,area;
+    cout<<"Enter base and height of triangle:";
+    cin>>b>>h;
+    area=(b*h)/2;
+    cout<<"Area of triangle is "<<area<<" sq cm";
+    return 0;
+}
