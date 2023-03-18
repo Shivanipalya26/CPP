6827ad730efa8869c616f1148182de024fc6e244
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2023-03-18 13:31:07 +0530

    Amount to be added

--- piggy.cpp
+++ piggy.cpp
@@ -4,23 +4,23 @@ class addAmount{
 	public:
 	static int amount;
 		addAmount(){
-			cout<<"Amount in piggy bank is " <<amount<<endl;
+			cout << "Amount in piggy bank is " << amount << endl;
 		}
 		addAmount(int a){
-			amount+=a;
+			amount += a;
 		}
 		void display()
 		{
-			cout<<"Amount in piggy bank is "<<amount;
+			cout << "Amount in piggy bank is " << amount;
 		}
 };
-int addAmount::amount=50;
+int addAmount::amount = 50;
 int main()
 {
 	int num;
 	clrscr();
-	cout<<"Enter amount to be added ";
-	cin>>num;
+	cout << "Enter amount to be added ";
+	cin >> num;
 	addAmount ob;
 	addAmount obj(num);
 	ob.display();
