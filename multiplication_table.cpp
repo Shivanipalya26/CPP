f64c07f5b7694e781093460f9fb4b9710daa329d
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2023-03-18 13:33:37 +0530

    Added file

---
+++ multiplication_table.cpp
@@ -0,0 +1,16 @@
+//to print multiplication table
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout << "Enter no.:";
+    cin >> n;
+    for(i = 1;i <= 10;i++)
+    {
+        cout << n << "*" << i << "=" << n*i << endl;
+    }
+    return 0;
+}
