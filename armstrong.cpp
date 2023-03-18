d2920cadb40b40101ca32ca7ff82e276ef059c95
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2023-03-18 13:58:44 +0530

    Added armstrong file

--- armstrong.cpp
+++ armstrong.cpp
@@ -3,19 +3,23 @@ using namespace std;
 
 int main()
 {
-    int r,n,sum=0,m;
-    cout<<"Enter num:";
-    cin>>n;
-    m=n;
-    for(;n!=0;)
+    int r,n,m,sum = 0;
+    cout << "Enter num:";
+    cin >> n;
+    m = n;
+    
+    for( ; n != 0 ; )
     {
-        r=n%10;
-        n/=10;
-        sum=sum+r*r*r;
+        r = n % 10;
+        n /= 10;
+        sum = sum + r*r*r;
     }
-    if(m==sum)
+    
+    if(m == sum)
     {
-        cout<<"Entered number is armstrong number";
+        cout << "Entered number is armstrong number";
     }
-    else cout<<"Entered number is not an amrstrong number";
+    else cout << "Entered number is not an amrstrong number";
+    
+    return 0;
 }
